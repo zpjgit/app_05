dae6d1d13968c381cda08f8d2c6dd170cbd35b0d
zpjgit <zpj.love@foxmail.com>
2018-03-19 15:29:39 +0800

    动态加载碎片

--- app/src/main/java/com/zpj/myapplication5/MainActivity.java
+++ app/src/main/java/com/zpj/myapplication5/MainActivity.java
@@ -2,21 +2,42 @@ package com.zpj.myapplication5;
 
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
 
-
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+        replaceFragment(new Right_Activity());
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                replaceFragment(new AnotherRightFragment());
+                break;
+            default:
+                break;
+        }
+    }
 
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_fragment, fragment);
+        transaction.commit();
+    }
 }
